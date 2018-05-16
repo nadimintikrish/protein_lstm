aa951dadb0fe11b83ac38b6a4d084f0812436437
Krishna Nadiminti <nadimintikrish@gmail.com>
2018-05-15 21:36:31 -0400

    simple modeling for first 1000 sequences

--- utilities/DataPreProcessor.py
+++ utilities/DataPreProcessor.py
@@ -23,11 +23,22 @@ class DataPreProcessor:
 
     def process_all_seqs(self, X, y):
         apply_one_hot_encoding = self.one_hot_encoder.apply_one_hot_encoding
-        #self.X_list_seq = list(map(apply_one_hot_encoding,filter(lambda x: len(x) <=1000,X)))
         self.X_list_seq = list(map(apply_one_hot_encoding, X))
         self.y = pd.get_dummies(y)
         self.protein_categories = list(self.y.columns)
         return self.X_list_seq
+
+    def process_seqs_to_arrays(self, X, y, max_length):
+        apply_one_hot_encoding = self.one_hot_encoder.apply_one_hot_encoding
+        temp_x = list(map(apply_one_hot_encoding, X))
+        for i in range(len(temp_x)):
+            rem_len = max_length - len(temp_x[i])
+            if rem_len > 0:
+                temp_x[i].extend([[0] * len(self.one_hot_encoder.aa_set)] * rem_len)
+        temp_y = pd.get_dummies(y)
+        self.protein_categories = list(temp_y.columns)
+        self.X_list_seq = np.array(temp_x)
+        self.y = np.array(temp_y)
 
     def decode_categories(self, encoded_array):
         place = np.argmax(encoded_array)
@@ -42,7 +53,7 @@ class DataPreProcessor:
             for i in range(len(sliced)):
                 rem_len = max_len - len(sliced[i])
                 if rem_len > 0:
-                    sliced[i].extend([[0] * len(self.one_hot_encoder.aa_set)] * rem_len) # to optimize
+                    sliced[i].extend([[0] * len(self.one_hot_encoder.aa_set)] * rem_len)  # to optimize
             yield np.array(sliced), np.array(y_sliced)
 
     def get_protein_categoeies(self):
